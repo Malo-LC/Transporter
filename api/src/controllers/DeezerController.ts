2f5fbcd62b5672178802cefd0e1a42c903396464
mlecorvec <mlecorvec@coreoz.com>
2025-05-28 14:10:04 +0200

    Enhance DeezerController with validation middleware for playlist export and file upload endpoints

--- api/src/controllers/DeezerController.ts
+++ api/src/controllers/DeezerController.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono';
 import { getSignedCookie } from 'hono/cookie';
+import { validator } from 'hono/validator';
 import { WebSocket } from 'ws';
 import { SECRET_COOKIE_KEY } from '../config';
 import { DeezerApiService } from '../service/DeezerApiService';
@@ -7,8 +8,9 @@ import { DeezerFileService } from '../service/DeezerFileService';
 import deezerService from '../service/DeezerService';
 import spotifyApiService from '../service/SpotifyApiService';
 import spotifyService from '../service/SpotifyService';
-import { CreateSpotifyPlaylistBody, DeezerTracks, TaskProgress, TrackData } from '../types/DeezerTypes';
+import { CreateSpotifyPlaylistBody, DeezerTracks, TrackData } from '../types/DeezerTypes';
 import deezerTaskProgressService from '../service/DeezerTaskProgressService';
+import { validateDeezerFilePlaylistExport, validateDeezerPlaylistExport } from '../validator/deezerValidator';
 
 type Context = {
   userId: string | undefined;
@@ -21,8 +23,6 @@ export type AugmentedDeezerController = Hono<{ Variables: Context }> & {
 
 const deezerController: AugmentedDeezerController = new Hono<{ Variables: Context }>() as AugmentedDeezerController;
 const deezerApiService = new DeezerApiService();
-
-const taskProgressStore = new Map<string, TaskProgress>();
 
 deezerController.use('*', async (c, next) => {
   const userId = await getSignedCookie(c, SECRET_COOKIE_KEY, 'userId');
@@ -47,33 +47,12 @@ Object.assign(deezerController, {
   unregisterWebSocketForTask,
 });
 
-deezerController.post('/start-playlist-export', async (c) => {
+deezerController.post('/start-playlist-export', validator('json', validateDeezerPlaylistExport), async (c) => {
   const t0 = performance.now();
 
-  const userId = c.get('userId');
   const {
-    name, description, public: isPublic, isLikes = false, playlistUrl
-  } = await c.req.json<CreateSpotifyPlaylistBody>();
-
-  const regex = /(?:playlist\/|)(\d+)(?:[/?]|$)/m;
-  const match = playlistUrl?.match(regex);
-  const playlistId = match ? match[1] : null;
-
-  if (!playlistId) {
-    return c.json({ message: 'Invalid Deezer playlist URL' }, 400);
-  }
-
-  if (!name && !isLikes) {
-    return c.json({ message: 'No playlist name provided' }, 400);
-  }
-
-  if (!userId) {
-    return c.json({ message: 'User ID is missing' }, 401);
-  }
-
-  if (!spotifyApiService.hasAccessToken(userId)) {
-    return c.json({ message: 'Spotify access token is missing' }, 401);
-  }
+    userId, name, description, isPublic, isLikes, playlistId
+  } = c.req.valid('json');
 
   const taskId = `transfer-${Date.now()}-${Math.random().toString(36).substring(2, 9)}`;
 
@@ -84,22 +63,23 @@ deezerController.post('/start-playlist-export', async (c) => {
     totalSongs: 0,
     webSocketClients: [],
   });
+  const deezerPlaylistTracks: DeezerTracks = await deezerApiService.fetchPlaylist(playlistId);
+
+  if (!deezerPlaylistTracks.data || deezerPlaylistTracks.data.length === 0) {
+    deezerTaskProgressService.updateTaskProgress(taskId, { status: 'error' });
+    return;
+  }
+
+  const deezerTracks: TrackData[] = deezerPlaylistTracks.data.map((track) => ({
+    trackName: track.title,
+    artistName: track.artist.name,
+    albumName: track.album.title,
+  }));
+
+  console.info('Fetched deezer playlist with ', deezerTracks.length, ' tracks for task', taskId);
 
   (async () => {
     try {
-      const deezerPlaylistTracks: DeezerTracks = await deezerApiService.fetchPlaylist(playlistId);
-
-      if (!deezerPlaylistTracks.data || deezerPlaylistTracks.data.length === 0) {
-        deezerTaskProgressService.updateTaskProgress(taskId, { status: 'error' });
-        return;
-      }
-
-      const deezerTracks: TrackData[] = deezerPlaylistTracks.data.map((track) => ({
-        trackName: track.title,
-        artistName: track.artist.name,
-        albumName: track.album.title,
-      }));
-
       deezerTaskProgressService.updateTaskProgress(taskId, {
         totalSongs: deezerTracks.length,
       });
@@ -110,9 +90,7 @@ deezerController.post('/start-playlist-export', async (c) => {
         deezerTaskProgressService.updateTaskProgress(taskId, { status: 'error' });
         return;
       }
-      deezerTaskProgressService.updateTaskProgress(taskId, {
-        spotifyPlaylistId,
-      });
+      deezerTaskProgressService.updateTaskProgress(taskId, { spotifyPlaylistId });
 
       const missingTracks = await deezerService.transferTracksToSpotify(
         userId,
@@ -141,7 +119,7 @@ deezerController.post('/start-playlist-export', async (c) => {
       });
 
     } catch (error) {
-      console.error(`Error during playlist transfer for task ${taskId}:`, error);
+      console.error(`Error during file playlist processing for task ${taskId}:`, error);
       deezerTaskProgressService.updateTaskProgress(taskId, {
         status: 'error',
       });
@@ -150,37 +128,17 @@ deezerController.post('/start-playlist-export', async (c) => {
         deezerTaskProgressService.deleteTask(taskId);
       }, 5 * 60 * 1000);
     }
-  })()
-    .then(r => r);
+  })().then(r => r);
 
   return c.json({ taskId }, 200);
 });
 
-deezerController.post('/file', async (c) => {
+deezerController.post('/file', validator('form', validateDeezerFilePlaylistExport), async (c) => {
   const t0 = performance.now();
-  const userId = c.get('userId');
-  const body = await c.req.parseBody();
 
-  const name = body.name as string | undefined;
-  const isLikesValue = body.isLikes;
-  const isLikes = typeof isLikesValue === 'string' ? isLikesValue.toLowerCase() === 'true' : !!isLikesValue;
-  const file = body.file as File | undefined;
-
-  if (!file) {
-    return c.json({ message: 'No file provided' }, 400);
-  }
-
-  if (!name && !isLikes) {
-    return c.json({ message: 'No playlist name provided' }, 400);
-  }
-
-  if (!userId) {
-    return c.json({ message: 'User ID is missing' }, 401);
-  }
-
-  if (!spotifyApiService.hasAccessToken(userId)) {
-    return c.json({ message: 'Spotify access token is missing' }, 401);
-  }
+  const {
+    userId, name, isLikes, file, description, isPublic
+  } = c.req.valid('form');
 
   const taskId = `file-transfer-${Date.now()}-${Math.random().toString(36).substring(2, 9)}`;
 
@@ -191,22 +149,25 @@ deezerController.post('/file', async (c) => {
     totalSongs: 0,
     webSocketClients: [],
   });
+  
+  const playlist = DeezerFileService.parseCsv(await file.text());
+
+  if (!playlist || playlist.tracks.length === 0) {
+    deezerTaskProgressService.updateTaskProgress(taskId, { status: 'error' });
+    return;
+  }
+
+  const deezerTracks: TrackData[] = playlist.tracks;
+
+  console.info('Fetched deezer playlist ', playlist.playlistName, ' with ', playlist.tracks.length, ' tracks for task', taskId);
 
   (async () => {
     try {
-      const playlist = DeezerFileService.parseCsv(await file.text());
-
-      if (!playlist || playlist.tracks.length === 0) {
-        deezerTaskProgressService.updateTaskProgress(taskId, { status: 'error' });
-        return;
-      }
-
-      console.info('Fetched deezer playlist ', playlist.playlistName, ' with ', playlist.tracks.length, ' tracks for task', taskId);
       deezerTaskProgressService.updateTaskProgress(taskId, {
-        totalSongs: playlist.tracks.length,
+        totalSongs: deezerTracks.length,
       });
 
-      const spotifyPlaylistId = await spotifyService.createOrGetSpotifyPlaylist(userId, name, !!isLikes);
+      const spotifyPlaylistId = await spotifyService.createOrGetSpotifyPlaylist(userId, name, isLikes, description, isPublic);
 
       if (!spotifyPlaylistId) {
         deezerTaskProgressService.updateTaskProgress(taskId, { status: 'error' });
@@ -216,11 +177,11 @@ deezerController.post('/file', async (c) => {
 
       const missingTracks = await deezerService.transferTracksToSpotify(
         userId,
-        playlist.tracks,
+        deezerTracks,
         spotifyPlaylistId,
-        !!isLikes,
+        isLikes,
         (currentProgress: number, songName: string) => {
-          const total = playlist.tracks.length;
+          const total = deezerTracks.length;
           const percentage = Math.round((currentProgress / total) * 100);
           deezerTaskProgressService.updateTaskProgress(taskId, {
             status: 'transferring',
@@ -231,8 +192,6 @@ deezerController.post('/file', async (c) => {
           });
         }
       );
-
-      console.info(`Successfully added all tracks to Spotify playlist "${spotifyPlaylistId}" for task ${taskId}.`);
 
       deezerTaskProgressService.updateTaskProgress(taskId, {
         status: 'completed',
@@ -250,7 +209,6 @@ deezerController.post('/file', async (c) => {
     } finally {
       setTimeout(() => {
         deezerTaskProgressService.deleteTask(taskId);
-        console.log(`Task ${taskId} (file processing) removed from store.`);
       }, 5 * 60 * 1000);
     }
   })().then(r => r);
---
+++ api/src/validator/deezerValidator.ts
@@ -0,0 +1,81 @@
+import type { Context } from 'hono';
+import { CreateSpotifyPlaylistBody } from '../types/DeezerTypes';
+import spotifyApiService from '../service/SpotifyApiService';
+
+export async function validateDeezerPlaylistExport<T, U extends Context>(_value: T, c: U) {
+  const userId: string = c.get('userId');
+
+  const {
+    name, description, public: isPublic = true, isLikes = false, playlistUrl,
+  } = await c.req.json<CreateSpotifyPlaylistBody>();
+
+  if (!playlistUrl) {
+    return c.json({ message: 'No playlist URL provided' }, 400);
+  }
+
+  const regex = /(?:playlist\/|)(\d+)(?:[/?]|$)/m;
+  const match = playlistUrl?.match(regex);
+  const playlistId = match ? match[1] : null;
+
+  if (!playlistId) {
+    return c.json({ message: 'Invalid Deezer playlist URL' }, 400);
+  }
+
+  if (!name && !isLikes) {
+    return c.json({ message: 'No playlist name provided' }, 400);
+  }
+
+  if (!userId) {
+    return c.json({ message: 'User ID is missing' }, 401);
+  }
+
+  if (!spotifyApiService.hasAccessToken(userId)) {
+    return c.json({ message: 'Spotify access token is missing' }, 401);
+  }
+
+  return {
+    userId,
+    name,
+    description,
+    isPublic,
+    isLikes,
+    playlistId,
+  };
+}
+
+export async function validateDeezerFilePlaylistExport<T, U extends Context>(_value: T, c: U) {
+  const userId: string = c.get('userId');
+  const body = await c.req.parseBody();
+
+  const name = body.name as string | undefined;
+  const isLikesValue = body.isLikes;
+  const isLikes = typeof isLikesValue === 'string' ? isLikesValue.toLowerCase() === 'true' : !!isLikesValue;
+  const file = body.file as File | undefined;
+  const description = body.description as string | undefined;
+  const isPublic = typeof body.isPublic === 'string' ? body.isPublic.toLowerCase() === 'true' : !!body.isPublic;
+
+  if (!file) {
+    return c.json({ message: 'No file provided' }, 400);
+  }
+
+  if (!name && !isLikes) {
+    return c.json({ message: 'No playlist name provided' }, 400);
+  }
+
+  if (!userId) {
+    return c.json({ message: 'User ID is missing' }, 401);
+  }
+
+  if (!spotifyApiService.hasAccessToken(userId)) {
+    return c.json({ message: 'Spotify access token is missing' }, 401);
+  }
+
+  return {
+    userId,
+    name,
+    description,
+    isPublic,
+    isLikes,
+    file,
+  };
+}
